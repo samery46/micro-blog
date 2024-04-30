e5b6558fff6474b6c584e06e5454962d700be952
samery46 <syamme@ymail.com>
2024-04-30 18:09:10 +0700

    Install Tailwind CSS

---
+++ src/Controller/PostController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    #[Route('/', methods: ['GET'], name: 'posts.index')]
+    public function index(): Response
+    {
+        return $this->render('post/index.html.twig');
+    }
+
+    #[Route('/post/new', methods: ['GET', 'POST'], name: 'posts.new')]
+    public function new(): Response
+    {
+        return $this->render('post/new.html.twig');
+    }
+
+    #[Route('/post/{id}', methods: ['GET'], name: 'posts.show')]
+    public function show($id): Response
+    {
+        return $this->render('post/show.html.twig');
+    }
+
+    #[Route('/post/{id}/delete', methods: ['POST'], name: 'posts.delete')]
+    public function delete($id): Response
+    {
+        return new Response('delete post from the database');
+    }
+
+    #[Route('/post/{id}/edit', methods: ['GET', 'POST'], name: 'posts.edit')]
+    public function edit($id): Response
+    {
+        // return $this->redirectToRoute('posts.index');
+
+        return $this->render('post/edit.html.twig');
+    }
+
+    #[Route('/posts/user/{id}', methods: ['GET'], name: 'posts.user')]
+    public function user($id): Response
+    {
+        // return new Response($id);
+        return $this->render('post/index.html.twig');
+    }
+
+
+    #[Route('/toggleFollow/{user}', methods: ['GET'], name: 'toggleFollow')]
+    public function toggleFollow($user): Response
+    {
+        // return new Response($user);
+        return new Response('logic for toggling like/dislike functionality');
+    }
+
+
+    // #[Route('/post', name: 'app_post')]
+    // public function index(): JsonResponse
+    // {
+    //     return $this->json([
+    //         'message' => 'Welcome to your new controller!',
+    //         'path' => 'src/Controller/PostController.php',
+    //     ]);
+    // }
+}
