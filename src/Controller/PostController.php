e3bc855f2adc4964d3a5f82e6d10c402c51c6cd6
samery46 <syamme@ymail.com>
2024-05-05 18:46:40 +0700

    Setting Database and Security

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,41 +6,44 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/', requirements: ['_locale' => 'en|id'])]
 class PostController extends AbstractController
 {
-    #[Route('/{_locale?}', methods: ['GET'], name: 'posts.index')]
-    public function index(): Response
+    #[Route('/{_locale}', methods: ['GET'], name: 'posts.index')]
+    public function index(string $_locale = 'en'): Response
     {
         return $this->render('post/index.html.twig');
     }
 
-    #[Route('/post/new', methods: ['GET', 'POST'], name: 'posts.new')]
+    #[Route('/{_locale}/post/new', methods: ['GET', 'POST'], name: 'posts.new')]
     public function new(): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return $this->render('post/new.html.twig');
     }
 
-    #[Route('/post/{id}/{_locale?}', methods: ['GET'], name: 'posts.show')]
+    #[Route('/{_locale}/post/{id}', methods: ['GET'], name: 'posts.show')]
     public function show($id): Response
     {
         return $this->render('post/show.html.twig');
     }
 
-    #[Route('/post/{id}/delete', methods: ['POST'], name: 'posts.delete')]
+    #[Route('/{_locale}/post/{id}/delete', methods: ['POST'], name: 'posts.delete')]
     public function delete($id): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return new Response('delete post from the database');
     }
 
-    #[Route('/post/{id}/edit', methods: ['GET', 'POST'], name: 'posts.edit')]
+    #[Route('/{_locale}/post/{id}/edit', methods: ['GET', 'POST'], name: 'posts.edit')]
     public function edit($id): Response
     {
         // return $this->redirectToRoute('posts.index');
-
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return $this->render('post/edit.html.twig');
     }
 
-    #[Route('/posts/user/{id}', methods: ['GET'], name: 'posts.user')]
+    #[Route('/{_locale}/posts/user/{id}', methods: ['GET'], name: 'posts.user')]
     public function user($id): Response
     {
         // return new Response($id);
@@ -48,10 +51,11 @@ class PostController extends AbstractController
     }
 
 
-    #[Route('/toggleFollow/{user}', methods: ['GET'], name: 'toggleFollow')]
+    #[Route('/{_locale}/toggleFollow/{user}', methods: ['GET'], name: 'toggleFollow')]
     public function toggleFollow($user): Response
     {
         // return new Response($user);
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return new Response('logic for toggling like/dislike functionality');
     }
 
